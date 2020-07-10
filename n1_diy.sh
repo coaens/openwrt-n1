dae9971966279aff14c042232a80d51128de5d3b
coaens <21050508+coaens@users.noreply.github.com>
2020-07-10 18:15:00 +0800

    fix

--- n1_diy.sh
+++ n1_diy.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.0.8/g' lede/package/base-files/files/bin/config_generate
-sed -i 's/root::0:0:99999:7:::/root:$1$mdYDHM5.$uxf/osp/VV0pr4RJyISOF.:18452:0:99999:7:::/g' lede/package/base-files/files/etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$mdYDHM5.$uxf\/osp\/VV0pr4RJyISOF.:18452:0:99999:7:::/g' lede/package/base-files/files/etc/shadow

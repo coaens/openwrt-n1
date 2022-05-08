055587f3a0da4e68666b8dc3265376e7467333f5
coaens <21050508+coaens@users.noreply.github.com>
2022-05-08 14:24:41 +0800

    update

---
+++ diy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/10.0.0.8/g' lede/package/base-files/files/bin/config_generate
+sed -i 's/root::0:0:99999:7:::/root:$1$mdYDHM5.$uxf\/osp\/VV0pr4RJyISOF.:18452:0:99999:7:::/g' lede/package/base-files/files/etc/shadow

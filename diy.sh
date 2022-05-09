7629bba233b58e071874902ecedeca61bb736527
coaens <21050508+coaens@users.noreply.github.com>
2022-05-09 21:23:41 +0800

    update

--- diy.sh
+++ diy.sh
@@ -7,4 +7,8 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.0.8/g' lede/package/base-files/files/bin/config_generate
+
+sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' lede/package/lean/autocore/Makefile
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic lede/package/luci-app-amlogic
+
 sed -i 's/root::0:0:99999:7:::/root:$1$mdYDHM5.$uxf\/osp\/VV0pr4RJyISOF.:18452:0:99999:7:::/g' lede/package/base-files/files/etc/shadow
